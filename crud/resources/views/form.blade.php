bec9d1ad7edd8857a05d81ce9c49a34d01ec49e6
Vipankumarpa <vipankumarpal18vkp@gmail.com>
2023-03-11 15:52:58 +0530

    Learn How To Use Components In Laravel

--- crud/resources/views/form.blade.php
+++ crud/resources/views/form.blade.php
@@ -17,52 +17,17 @@
         print_r($errors->all());
         @endphp
         </pre> --}}
-        <div class="mb-3">
-            <label for="" class="form-label">Name</label>
-            <input type="text"
-            class="form-control" name="name" id="" aria-describedby="helpId" placeholder="Enter Your Full Name" value="{{old('name')}}">
-            <span class="text-danger">
-                @error('name')
-                    {{$message}}
-                @enderror
-            </span>
-            {{-- <small id="helpId" class="form-text text-muted">Help text</small> --}}
-        </div>
-        <div class="mb-3">
-            <label for="" class="form-label">Email</label>
-            <input type="email"
-            class="form-control" name="email" id="" aria-describedby="helpId" placeholder="Enter Your Email Here"  value="{{old('email')}}">
-            <span class="text-danger">
-                @error('email')
-                    {{$message}}
-                @enderror
-            </span>
-            {{-- <small id="helpId" class="form-text text-muted">Help text</small> --}}
-        </div>
 
-        <div class="mb-3">
-            <label for="" class="form-label">Password</label>
-            <input type="password"
-            class="form-control" name="password" id="" aria-describedby="helpId" placeholder="Enter Your Password Here">
-            <span class="text-danger">
-                @error('password')
-                    {{$message}}
-                @enderror
-            </span>
-            {{-- <small id="helpId" class="form-text text-muted">Help text</small> --}}
-        </div>
-
-        <div class="mb-3">
-            <label for="" class="form-label">Confirm Password</label>
-            <input type="password"
-            class="form-control" name="password_confirmation" id="" aria-describedby="helpId" placeholder="Confirm Your Password Here">
-            <span class="text-danger">
-                @error('password_confirmation')
-                    {{$message}}
-                @enderror
-            </span>
-            {{-- <small id="helpId" class="form-text text-muted">Help text</small> --}}
-        </div>
+        {{-- @php
+            $demo=1;
+        @endphp --}}
+        {{-- <x-input type="text" name="name" label="Please Enter Your Name Here" :demo="$demo" /> --}}
+        <x-input type="text" name="name" label="Please Enter Your Name Here"  />
+        <x-input type="email" name="email" label="Please Enter Your Email Here" />
+        <x-input type="password" name="password" label="Password" />
+        <x-input type="password" name="password_confirmation" label="Confirm Password" />
+        
+        
         <button class="btn btn-primary">Submit</button>
     </form>
 
